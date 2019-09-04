4e4037b101874118f4f039013a60d86674102d1b
expert7kr <expert7kr@naver.com>
2019-09-04 17:03:00 +0900

    코드수정 React - 12. props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class TOC extends Component {
+  render(){
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render(){
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
 class Subject extends Component {
   render(){
     return (
       <header>
-        <h1>Web</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
@@ -16,7 +41,9 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="Web" sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
