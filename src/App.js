b2336c3c8436d12156658dc0a94f5b4f4e6ddbfe
expert7kr <expert7kr@naver.com>
2019-09-05 07:25:45 +0900

    주석삭제

--- src/App.js
+++ src/App.js
@@ -38,17 +38,6 @@ class App extends Component {
           }.bind(this)}
         >
         </Subject>
-        {/* <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault();
-            // this.state.mode='welcome';
-            this.setState({
-              mode:'welcome'
-            });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header> */}
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
