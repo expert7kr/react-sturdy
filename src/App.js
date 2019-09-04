8f54741c658409dc9cbaf9e698d9ac6bb58cb9eb
expert7kr <expert7kr@naver.com>
2019-09-05 05:17:28 +0900

    에러 발생으로 강의와 수업이 달라짐 React - 16.1. 이벤트 state props 그리고 render 함수 }else if(this.state.mod==='read'){ >> }else {

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode:'read',
       subject:{title:'Web', sub:'World wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, react!!'},
       contents:[
         {id:1, title:"HTML", desc:'HTML is for information'},
         {id:2, title:"CSS", desc:'CSS is for design'},
-        {id:1, title:"JavaScript", desc:'JavaScript is for interractive'}
+        {id:3, title:"JavaScript", desc:'JavaScript is for interractive'}
       ]
     }
   }
   render(){
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode==='welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else{
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
@@ -24,7 +35,7 @@ class App extends Component {
           sub={this.state.subject.sub}>
         </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
