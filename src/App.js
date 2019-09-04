4e5acf3a5907ba2c98eb3d8476696306f1b5553f
expert7kr <expert7kr@naver.com>
2019-09-05 08:01:11 +0900

    코드변경 React - 17.2. 컴포넌트 이벤트 만들기

--- src/App.js
+++ src/App.js
@@ -38,7 +38,13 @@ class App extends Component {
           }.bind(this)}
         >
         </Subject>
-        <TOC data={this.state.contents}></TOC>
+        <TOC  
+          data={this.state.contents}
+          onChangePage={function(){
+            this.setState({mode:'read'});
+          }.bind(this)}
+        >
+        </TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
