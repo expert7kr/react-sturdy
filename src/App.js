3f57e51d2f44017d5fccb60b0fed5e300ec8286e
expert7kr <expert7kr@naver.com>
2019-09-05 05:22:28 +0900

    에러수정 >> 아래 Content 에 내용 나옴 >> 수업과 달라짐

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     if(this.state.mode==='welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    }else if(this.state.mod==='read'){
+    }else{
       _title = this.state.contents[0].title;
       _desc = this.state.contents[0].desc;
     }
