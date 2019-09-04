6638194017babf43429d2abaaafe3e7085d15c1c
expert7kr <expert7kr@naver.com>
2019-09-04 21:41:17 +0900

    코드수정 React - 15.3. key

--- src/App.js
+++ src/App.js
@@ -4,48 +4,26 @@ import Content from './components/Content';
 import Subject from './components/Subject';
 import './App.css';
 
-// class TOC extends Component {
-//   render(){
-//     return (
-//       <nav>
-//         <ul>
-//           <li><a href="1.html">HTML</a></li>
-//           <li><a href="2.html">CSS</a></li>
-//           <li><a href="3.html">JavaScript</a></li>
-//         </ul>
-//       </nav>
-//     );
-//   }
-// }
-
-// class Subject extends Component {
-//   render(){
-//     return (
-//       <header>
-//         <h1>{this.props.title}</h1>
-//         {this.props.sub}
-//       </header>
-//     );
-//   }
-// }
-
-// class Content extends Component {
-//   render(){
-//     return (
-//       <article>
-//         <h2>{this.props.title}</h2>
-//         {this.props.desc}
-//       </article>
-//     );
-//   }
-// }
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'Web', sub:'World wide Web!'},
+      contents:[
+        {id:1, title:"HTML", desc:'HTML is for information'},
+        {id:2, title:"CSS", desc:'CSS is for design'},
+        {id:1, title:"JavaScript", desc:'JavaScript is for interractive'}
+      ]
+    }
+  }
   render(){
     return (
       <div className="App">
-        <Subject title="Web" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
