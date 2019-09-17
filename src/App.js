899cdbde64a8d283f071248c8de3984814a3bf86
expert7kr <expert7kr@naver.com>
2019-09-17 15:10:57 +0900

    React - 20.1. update 구현 > 1. UpdateContent 만들고 App 에 반영

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
 import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Subject from './components/Subject';
 import Control from './components/Control';
 import './App.css';
-import CreateContent from './components/CreateContent';
 
 class App extends Component {
   constructor(props){
@@ -41,8 +42,8 @@ class App extends Component {
         i = i + 1;
       }
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    }else{ 
-      // else if(this.state.mode === 'create'){
+    // }else{ 
+    }else if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
         // add content to this.state.contents
         this.max_content_id = this.max_content_id+1;
@@ -60,6 +61,25 @@ class App extends Component {
         });   
         console.log(_title,_desc);
       }.bind(this)}></CreateContent>
+    }else{ 
+    // }else if(this.state.mode === 'update'){
+        _article = <UpdateContent onSubmit={function(_title, _desc){
+          // add content to this.state.contents
+          this.max_content_id = this.max_content_id+1;
+          // this.state.contents.push(
+          //   {id:this.max_content_id, title:_title, desc:_desc}
+          // );
+          // var _contents = this.state.contents.concat(
+          //   {id:this.max_content_id, title:_title, desc:_desc}
+          // );
+          var newContents = Array.from(this.state.contents);
+          newContents.push({id:this.max_content_id, title:_title, desc:_desc})
+          this.setState({
+            // contents:_contents
+            contents:newContents
+          });   
+          console.log(_title,_desc);
+        }.bind(this)}></UpdateContent>
     }
     return (
       <div className="App">
